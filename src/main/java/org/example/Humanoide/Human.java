53709eefb6998a1569b19f008fd21fe2a8282f65
fred <ton_email@example.com>
2025-03-28 16:42:19 +0100

    add Human claa

---
+++ src/main/java/org/example/Humanoide/Human.java
@@ -0,0 +1,12 @@
+package org.example.Humanoide;
+
+public class Human extends Humanoide {
+
+  public Human(String name, int age, String gender, int hp, int mp) {
+    super(name, age, gender, hp, mp);
+  }
+
+  public String getName() {
+    return name;
+  }
+}
