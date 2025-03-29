797aa2cf6085d5f687b7355b55d3c38ded7a38f6
fred <frednobre18@gmail.com>
2025-03-29 22:57:28 +0100

    add Dwarf class

---
+++ src/main/java/org/example/Humanoide/Dwarf.java
@@ -0,0 +1,8 @@
+package org.example.Humanoide;
+
+public class Dwarf extends Humanoide {
+
+  public Dwarf(String name, int age, String gender, int hp, int mp) {
+    super(name, age, gender, hp, mp);
+  }
+}
