dece38b096e5038081b9c332bf95a50bf9868212
Fred Nobre <frednobre18@gmail.com>
2025-04-16 16:16:19 +0200

    :sparkles: add a new class GameOrganisation

---
+++ src/main/java/org/example/Race/GameOrganisation.java
@@ -0,0 +1,13 @@
+package org.example.Race;
+
+import java.util.List;
+
+public class GameOrganisation {
+    private int dice;
+    private String player1Name;
+    private String player2Name;
+    private List[] caractersTeamsPlayer1;
+    private List[] caractersTeamsPlayer2;
+
+
+}
