ac2c44a599e1a2483e85cc56f84f49263b1fcd0a
Fred Nobre <frednobre18@gmail.com>
2025-04-16 16:13:20 +0200

    :recycle: refacto behavior interface

--- src/main/java/org/example/Race/Behavior.java
+++ src/main/java/org/example/Race/Behavior.java
@@ -3,9 +3,7 @@ package org.example.Race;
 public interface Behavior {
   String speak(String name);
 
-  String drink();
+  String drink(int value);
 
-  String escape();
-
-  String eat();
+  String eat(int value);
 }
