33129e58c440292c4455d724fe3c5de40cb0b782
Fred Nobre <frednobre18@gmail.com>
2025-04-16 16:20:54 +0200

    :construction: wip

--- src/main/java/org/example/Race/Main.java
+++ src/main/java/org/example/Race/Main.java
@@ -6,14 +6,16 @@ public class Main {
 
 
 
-    Human a = new Human("fred",12,"M",500,0,200,300,20);
-    Human b = new Human("dodo",12,"M",500,0,200,300,20);
+    Human a = new Human("fred",12,"M",500,0,20,300,20);
+    Human b = new Human("dodo",12,"M",500,0,20,300,20);
 
-    System.out.println(b.getHealthPoint());
-    a.bigAttack(b,a.getAttackPoint());
-    System.out.println(a.getAttackPoint());
+    System.out.println(a.getHealthPoint());
     System.out.println(b.getHealthPoint());
 
+    a.bigAttack(b,5);
+    System.out.println(b.getHealthPoint());
+    System.out.println(b.getHealthPoint());
 
+    System.out.println(b.drink(2));
   }
 }
